1d60c2de79b313e7b857bdacd455adfa5fcf2730
mounika338-ux <paravasthu338@gmail.com>
2025-10-19 09:12:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import DemoComponent from './DemoComponent';
+import TestComponent from './TestComponent';
+
+function App() {
+  return (
+    <div>
+      welcome to React js
+      <h1>Hello world</h1>
+      <p>This is a paragraph</p>
+      <button>onClick</button>
+      <DemoComponent />
+      <TestComponent />
+
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/DemoComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TestComponent from './TestComponent';
+
+const DemoComponent = () => {
+  return (
+    <div>
+      <h2>DemoComponent</h2>
+    <TestComponent  />
+    </div>
+  )
+}
+
+
+export default DemoComponent
